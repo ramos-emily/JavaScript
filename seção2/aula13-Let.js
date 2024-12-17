a1bfd75d5bf0128c6c0215fdc0d3368548fa9b33
Emily <ramos.emilyvi@gmail.com>
2024-12-17 09:25:56 -0300

    variaveis let

---
+++ seção2/aula13-Let.js
@@ -0,0 +1,13 @@
+// let nome = 'joao'; //let criar variaveis
+
+
+// console.log(nome,'nasceu em 1984. ');
+// console.log('em 2000 joao conhece maria. ');
+// console.log('joao casou-se com maria em 2012. ');
+// console.log('maria teve o 1 filho com joao em 2015. ');
+// console.log('o filho de joao se chama eduardo. ');
+
+let nome; //declarou a variavel
+nome = 'qualquer valor'; //inicailizando e mudando o valor
+console.log(nome);
+
