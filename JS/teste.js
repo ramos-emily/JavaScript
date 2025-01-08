8bf5d46ebd95dd070f4378b8a2b24ae45a9acaa9
Emily <ramos.emilyvi@gmail.com>
2025-01-08 09:21:42 -0300

    aulaplinio

---
+++ JS/teste.js
@@ -0,0 +1,94 @@
+//precisa do node js, veja taraves do terminal usando: nmp --v
+//DIO., W3school  ver plataforma
+
+
+
+//ver sobre:
+
+//onclick
+
+
+
+
+
+//array
+
+// const listname = new Array(
+//     "gaibriel",
+//     "jenifer"
+// )
+
+// console.log(listname)
+
+// listname.push('plinio')
+
+// console.log(listname)
+
+// console.log("nome:" + listname[1])
+
+
+
+
+
+//if else
+
+// const idade = 20
+// const idade2 = 18
+
+// if(idade < 18 || idade === 18) {
+//     console.log("n")
+// } 
+// else{
+//     console.log("crime")
+// }
+
+
+
+
+
+//operador ternario
+
+// let cont =0;
+
+// let verif = cont > 10 ? "cont maior q 110" : "cont menor" // "?" ve se é vdd, ":"" é ou
+
+// console.log(verif)
+
+
+
+
+
+//while
+
+// let cont = 0;
+
+// let rest = 10;
+
+// while(true){
+//     console.log(`vou aparecer mais: ${rest}`) //usa crase com sifrao e coloca variavel dentro das chaves
+//     rest--
+//     cont++
+//     if(rest === 0){ // = atribui valor, == comparando valor, === comparação estritamente igual (tipo dele)
+//         console.log("acabou")
+//         break;
+//     }
+// }
+
+
+
+
+//for
+
+
+// let cont = 0;
+// let restante = 11;
+
+// for(cont; cont < restante; cont++){
+//     console.log(cont)
+
+// }
+
+
+
+
+
